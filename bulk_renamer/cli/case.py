77c71afbe6337345bb29d02a7da734d203004b17
Filipe <filipediascontato@gmail.com>
2021-05-12 21:35:47 -0300

    Implement case command group

--- bulk_renamer/cli/case.py
+++ bulk_renamer/cli/case.py
@@ -1,33 +1,148 @@
+from pathlib import Path
+
 import typer
+
+from ..utils.functions import get_cwd_file_paths, rename_file
+
+"""Formats filename chars case based on the subcommands."""
 
 app = typer.Typer()
 
 
 @app.command()
-def alternate():
-    print("Calling case alternate command...")
+def alternate() -> None:
+    """Alternate the name characters between upper and lowercase."""
+
+    paths = get_cwd_file_paths()
+
+    for path in paths:
+        name = path.stem
+        extension = path.suffix
+
+        if not name:
+            continue
+
+        formated_name = ""
+        upper = True
+
+        for char in name:
+            formated_name += char.upper() if upper else char.lower()
+            upper = not upper
+
+        filename = f"{formated_name}{extension}"
+
+        rename_file(path, filename)
 
 
 @app.command()
-def camel():
-    print("Calling case camel command...")
+def camel(
+    whitespace: bool = typer.Option(
+        False, "--whitespace", "-w", help="Maintains the filename whitespaces."
+    )
+) -> None:
+    """Format the filename to camel case convention."""
+
+    paths = get_cwd_file_paths()
+
+    for path in paths:
+        name = path.stem
+        extension = path.suffix
+
+        if not name:
+            continue
+
+        name = name.lower()
+        name = name.title()
+        name = name[:1].lower() + name[1:]
+
+        if not whitespace:
+            name = name.replace(" ", "")
+
+        filename = f"{name}{extension}"
+
+        rename_file(path, filename)
 
 
 @app.command()
-def lower():
-    print("Calling case lower command...")
+def lower() -> None:
+    """Set the filename to lowercase."""
+
+    paths = get_cwd_file_paths()
+
+    for path in paths:
+        name = path.stem
+        extension = path.suffix
+
+        if not name:
+            continue
+
+        name = name.lower()
+        filename = f"{name}{extension}"
+
+        rename_file(path, filename)
 
 
 @app.command()
-def pascal():
-    print("Calling case pascal command...")
+def pascal(
+    whitespace: bool = typer.Option(
+        False, "--whitespace", "-w", help="Maintains the filename whitespaces."
+    )
+) -> None:
+    """Format the filename to pascal case convention."""
+
+    paths = get_cwd_file_paths()
+
+    for path in paths:
+        name = path.stem
+        extension = path.suffix
+
+        if not name:
+            continue
+
+        name = name.title()
+
+        if not whitespace:
+            name = name.replace(" ", "")
+
+        filename = f"{name}{extension}"
+
+        rename_file(path, filename)
 
 
 @app.command()
-def upper():
-    print("Calling case upper command...")
+def upper() -> None:
+    """Set the filename to uppercase."""
+
+    paths = get_cwd_file_paths()
+
+    for path in paths:
+        name = path.stem
+        extension = path.suffix
+
+        if not name:
+            continue
+
+        name = name.upper()
+        filename = f"{name}{extension}"
+
+        rename_file(path, filename)
 
 
 @app.command()
-def snake():
-    print("Calling case snake command...")
+def snake() -> None:
+    """Format the filename to snake case convention."""
+
+    paths = get_cwd_file_paths()
+
+    for path in paths:
+        name = path.stem
+        extension = path.suffix
+
+        if not name:
+            continue
+
+        name = name.lower()
+        name = name.replace(" ", "_")
+        filename = f"{name}{extension}"
+
+        rename_file(path, filename)
--- bulk_renamer/utils/functions.py
+++ bulk_renamer/utils/functions.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from pathlib import Path
+from os import path
+from random import randint
 
 
 def get_cwd_file_paths() -> list[Path]:
@@ -15,4 +17,8 @@ def get_cwd_file_paths() -> list[Path]:
 def rename_file(file_path: Path, new_filename: str) -> None:
     """Rename a file of a give path."""
 
+    extension = path.splitext(new_filename)[1]
+    temp_name = f"tempfile_{randint(1000, 9999)}{extension}"
+
+    file_path = file_path.rename(Path(file_path.parent, temp_name))
     file_path.rename(Path(file_path.parent, new_filename))
